c9c4da4fc7bef8f78734e11e0f803b33e85fbba9
Mohammed-ali75 <m.e.d.o.2.5.m7@gmail.com>
2024-11-13 19:27:40 +0200

    Updates

---
+++ static/js/952.cc8f03f6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktraining1=self.webpackChunktraining1||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.cc8f03f6.chunk.js.map
